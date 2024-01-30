8c3e965974d3989b8f2ee62061d3f7247ac230c2
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2024-01-29 20:21:18 -0500

    first init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [SerializeField] private float time = 0.1f;
+    [SerializeField] private bool isPlayerTurn = true; 
+
+    public bool IsPlayerTurn { get => isPlayerTurn; }
+
+    void Awake()
+    {
+     if(instance == null)
+        {
+            instance = this;
+        }   
+     else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        Instantiate(Resources.Load<GameObject>("Player")).name = "Player";
+    }
+
+    public void EndTurn() 
+    { 
+        isPlayerTurn = false;
+        StartCoroutine(WaitForTurns());
+    }
+
+    private IEnumerator WaitForTurns() 
+    {
+        yield return new WaitForSeconds(time);
+        isPlayerTurn=true;
+    }
+
+}
